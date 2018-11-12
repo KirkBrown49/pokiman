4e3b2931325cced9fc6c1dcfeb6652421bb4792a
Kirk Brown <kirkbrown@macbook-pro-97.local>
2018-11-12 13:38:09 -0600

    "set textview equal to JSON"

--- pokiman/ViewController.swift
+++ pokiman/ViewController.swift
@@ -14,10 +14,10 @@ class ViewController: UIViewController {
     // outlets for storyBoard
     @IBOutlet weak var pokimonNameTextField: UITextField!
     @IBOutlet weak var infoTextView: UITextView!
-    
+     let pokiApIBaseURL = "https://pokeapi.co/api/v2/pokemon/"
     override func viewDidLoad() {
         // base url for the pokimon api
-        let pokiApiBaseURL = "https://pokeapi.co/api/v2/"
+      
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
@@ -27,15 +27,31 @@ class ViewController: UIViewController {
         pokimonNameTextField.resignFirstResponder()
         
         //checking to make sure the field has value
-        guard let pokiName = pokimonNameTextField.text
+        guard let pokiName = pokimonNameTextField.text,
+                pokimonNameTextField.text != ""
             else {
                 return
         }
         //clearing the text field
         pokimonNameTextField.text = ""
         
-        //replacing spaces in name/title with + so they can be used as part of the url
-        let pokiNameURLComponent = pokiName.replacingOccurrences(of: " ", with: "+")
+        //replacing spaces in name with + so it can be used as part of the url
+        let pokiNameURLComponent = pokiName.replacingOccurrences(of: " ", with: "")
+        
+        //building our complete request url with name
+        let requestURL = pokiApIBaseURL + pokiNameURLComponent + "/"
+        
+        Alamofire.request(requestURL).responseJSON { (response) in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                self.infoTextView.text = json.rawString()
+            case .failure(let error):
+                self.infoTextView.text = "Invalid selection entered or an error occured.Please try again!"
+                print(error.localizedDescription)
+            }
+        }
+        
     }
     
 }
